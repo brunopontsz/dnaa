26f1ab61ac3148e86350e5a18a8cf274d91627f5
Pedro Lima <pedrohblima03@gmail.com>
2022-04-16 17:52:41 -0300

    Page in building completed

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const root = document.documentElement;
+const imagesDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed")
+const carousel_list = document.querySelector('ul.carousel_list');
+
+root.style.setProperty("--marquee-elements", carousel_list.children.length);
+
+for(let i = 0; i < imagesDisplayed; i++){
+  carousel_list.appendChild(carousel_list.children[i].cloneNode(true));
+}
+
+
+console.log("lorem")
